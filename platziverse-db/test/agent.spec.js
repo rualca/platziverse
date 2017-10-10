c279bb589205d4f8d323b839da5e02841736adf8
rualca <rualca@example.com>
2017-10-10 20:05:28 +0200

    Primera prueba unitaria con ava

---
+++ platziverse-db/test/agent.spec.js
@@ -0,0 +1,18 @@
+'use strict';
+
+const test = require('ava');
+
+const config = {
+	logging: () => {}
+};
+
+let db = null;
+
+test.beforeEach(async () => {
+	const setupDatabase = require('../');
+	db = await setupDatabase(config);
+});
+
+test('Agent', t => {
+	t.truthy(db.Agent, 'Agent service should exist');
+});
