68a528853e0c4a5fed717f0b95bcde8ecf4ad1b1
rualca <rualca@example.com>
2017-10-15 14:24:05 +0200

    Implementación del servicio Metric

--- platziverse-db/index.js
+++ platziverse-db/index.js
@@ -4,6 +4,7 @@ const defaults = require('defaults');
 const setupDatabase = require('./lib/db');
 const setupAgentModel = require('./models/agent');
 const setupAgent = require('./lib/agent');
+const setupMetric = require('./lib/metric');
 const setupMetricModel = require('./models/metric');
 
 module.exports = async function (config) {
@@ -33,7 +34,7 @@ module.exports = async function (config) {
 	}
 
 	const Agent = setupAgent(AgentModel);
-	const Metric = {};
+	const Metric = setupMetric(MetricModel, AgentModel);
 
 	return {
 		Agent,
