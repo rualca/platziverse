e7c25593daf0e68083538a6359405589aa9c6ddf
rualca <rualca@example.com>
2017-10-21 11:30:04 +0200

    Añadidos manejadores de eventos

--- platziverse-mqtt/server.js
+++ platziverse-mqtt/server.js
@@ -1,6 +1,5 @@
 'use strict';
 
-const debug = require('debug')('platziverse:mqtt');
 const mosca = require('mosca');
 const redis = require('redis');
 const chalk = require('chalk');
@@ -20,5 +19,28 @@ const server = new mosca.Server(settings);
 
 server.on('ready', () => {
   console.log(`${chalk.green('[platziverse-mqtt]')} server is running`);
-  
 });
+
+server.on('clientConnected', (client) => {
+  console.log(`Client Connected: ${client.id}`);
+});
+
+server.on('clientDisconnected', (client) => {
+  console.log(`Client Diconnected: ${client.id}`);
+});
+
+server.on('published', (packet, client) => {
+  console.log(`Received: ${packet.topic}`);
+  console.log(`Payload: ${packet.payload}`);
+});
+
+
+const handleFatalError = function(err) {
+  console.error(`${chalk.red('[faltal error]')} ${err.message}`);
+  console.error(`${err.stack}`);
+  process.exit(1);
+};
+
+server.on('error', handleFatalError);
+process.on('uncaughtException', handleFatalError);
+process.on('unhanledRejection', handleFatalError);
